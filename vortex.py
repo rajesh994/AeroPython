1f05cdbc105cff3cc73cb99ee72d2aac403841d9
Ramesh <rajesh.ramesh@capgemini.com>
2018-05-11 18:19:41 +0530

    Add vortex

--- vortex.py
+++ vortex.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri May 11 17:06:46 2018
+Created on Fri May 11 17:15:16 2018
 
 @author: rajerame
 """
@@ -9,15 +9,21 @@ import math
 import numpy
 from matplotlib import pyplot
 
+
+
+gamma = 5.0                      # strength of the vortex
+Nv=12                   #number of vortices
+a=5                     #distance between vortices
+x_vortex=numpy.arange(0,Nv*a,a)
+y_vortex=numpy.zeros(Nv) # location of the vortex
+
 N = 50                                # Number of points in each direction
-x_start, x_end = -2.0, 2.0            # x-direction boundaries
-y_start, y_end = -1.0, 1.0            # y-direction boundaries
+x_start, x_end = 0, 5*a            # x-direction boundaries
+y_start, y_end = -a,a            # y-direction boundaries
 x = numpy.linspace(x_start, x_end, N)    # computes a 1D-array for x
 y = numpy.linspace(y_start, y_end, N)    # computes a 1D-array for y
 X, Y = numpy.meshgrid(x, y)              # generates a mesh grid
 
-gamma = 5.0                      # strength of the vortex
-x_vortex, y_vortex = 0.0, 0.0    # location of the vortex
 
 def get_velocity_vortex(strength, xv, yv, X, Y):
     """
@@ -75,3 +81,28 @@ def get_stream_function_vortex(strength, xv, yv, X, Y):
     
     return psi
 
+
+v=numpy.zeros((N, N), dtype=float)
+u=numpy.zeros((N, N), dtype=float)
+
+for x in x_vortex:
+    for y in y_vortex:
+        u=u+get_velocity_vortex(gamma, x, y, X, Y)[0]
+        v=v+get_velocity_vortex(gamma, x, y, X, Y)[1]
+        
+
+
+
+
+
+# plot the streamlines
+width = 10
+height = (y_end - y_start) / (x_end - x_start) * width
+pyplot.figure(figsize=(width, height))
+pyplot.xlabel('x', fontsize=16)
+pyplot.ylabel('y', fontsize=16)
+pyplot.xlim(x_start, x_end)
+pyplot.ylim(y_start, y_end)
+pyplot.streamplot(X, Y, u, v,
+                 density=2, linewidth=1, arrowsize=1, arrowstyle='->')
+pyplot.scatter(x_vortex, y_vortex, color='#CD2305', s=80, marker='o');
